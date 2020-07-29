7b3d275636dd73b63345c2af234728b608ea0e67
SouzaFabiano <67329184+fabi47@users.noreply.github.com>
2020-07-30 01:20:23 +0900

    Update fizzbuzz_2.php
    修正
    
    最後の「　.　」を消しました。

--- fizzbuzz_2.php
+++ fizzbuzz_2.php
@@ -2,18 +2,21 @@
 
 function fizzbuzz($fizz, $buzz, $fizzbuzz){
 
-    for ($i = 1; $i < 100; $i++) {
+    for ($i = 1; $i < 100; $i++){
 
-        if ($i % $fizzbuzz === 0) {
-            echo 'FizzBuzz' . ".";
-        } elseif ($i % $fizz === 0) {
-            echo 'Fizz' . ".";
-        } elseif ($i % $buzz === 0) {
-            echo 'Buzz' . ".";
-        } else {
-            echo $i . ".";
+        if($i != 1){
+          echo ".";
         }
-
+        
+        if($i % $fizzbuzz === 0){
+            echo 'FizzBuzz';
+        }elseif ($i % $fizz === 0){
+            echo 'Fizz';
+        }elseif ($i % $buzz === 0){
+            echo 'Buzz';
+        }else{
+          echo $i;
+        }
     }
 
 }
