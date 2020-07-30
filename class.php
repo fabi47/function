1497f5c8a76fa23b7a4d9c53f3b308f002032dd9
fabi47 <fabi.47jsc@icloud.com>
2020-07-31 01:26:30 +0900

    class

---
+++ class.php
@@ -0,0 +1,87 @@
+<?php
+
+$min = 1;
+$max = 10;
+
+$fizz = 3;
+$buzz = 5;
+$fizzbuzz = 15;
+
+class HomeWork{
+
+//プロパティの宣言
+
+    public $kake = "1~10までの積\n";
+
+    public function kake($min, $max){
+        $total = 1;
+
+        for($i = $min; $i < $max; $i++){
+            $total *= $i;
+        }
+
+            return $total . "\n";
+    }
+
+
+    public $tasu = "1~10までの和\n";
+
+    public function tasu($min, $max){
+        $c = 0;
+
+        for ($i = $min; $i < $max; $i++){
+            $c += $i;
+        }
+
+            return $c . "\n";
+    }
+
+
+    public $tasu2 = "21~100までの和\n";
+/*
+    $min = 21;
+    $max = 100;
+
+    $ss = tasu($min, $max);
+*/
+
+    public $fizzbuzzq = "fizzbuzz問題\n";
+
+    public function fizzbuzz($fizz, $buzz, $fizzbuzz){
+
+        for ($i = 1; $i < 100; $i++){
+
+            if($i != 1){
+                echo ".";
+            }
+
+            if($i % $fizzbuzz === 0){
+                echo 'FizzBuzz';
+            }elseif ($i % $fizz === 0){
+                echo 'Fizz';
+            }elseif ($i % $buzz === 0){
+                echo 'Buzz';
+            }else{
+                echo $i;
+            }
+        }
+    }
+
+}
+
+//インスタンスの生成
+$homework = new HomeWork();
+
+    echo $homework->kake;
+    echo $homework->kake($min, $max);
+
+    echo $homework->tasu;
+    echo $homework->tasu($min, $max);
+
+    echo $homework->tasu2;
+    //echo $homework->;
+
+    echo $homework->fizzbuzzq;
+    echo $homework->fizzbuzz($fizz, $buzz, $fizzbuzz);
+
+ ?>
